1a2a7e31f8dcfe903f987a4badb76c9f6f276077
corrededos <michael.ginart@gmail.com>
2022-12-12 15:42:10 +0100

    JGI-2 Breaking change, field is changed/added
    JGI-2 Breaking change, field is changed/added

--- src/formansmapp.java
+++ src/formansmapp.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 public class formansmapp{
 
+    protected Mono<Forsakring> fetchSingleInsurance(UUID employmentId, AnstallningSelectable... selects) {
+        var filter = insuranceIdQueryFilter(insuranceId).and(insuranceTypeFilter);
+        return fetchInsurance(true, filter, selects).next();
+    }
 }
